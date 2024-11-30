ca02726cf2e0c8f1135819950f7618d7e9dd6747
mmyousefies <mmyousefies@gmail.com>
2024-12-01 02:47:57 +0330

    last one

--- src/package-management.R
+++
@@ -1,9 +0,0 @@
-package_names.vec <- c("glue","tidyverse")
-package_prep <- \(names.vec)
-{
-  for (name in names.vec)
-  {
-    if ( !require(name,character.only = TRUE)) renv::install(name)
-  }
-}
-package_names.vec |> package_prep()
--- src/unzip.R
+++
@@ -1,9 +0,0 @@
-if (!"package:tidyverse" %in% search()) library(tidyverse)
-if (!file.exists('intermediate-data')) dir.create('intermediate-data')
-list.files(
-  file.path('data'),
-  pattern = "\\.zip$",
-  full.names = T
-  ) |> 
-  walk(~unzip(.x,exdir = file.path('intermediate-data')))
-
